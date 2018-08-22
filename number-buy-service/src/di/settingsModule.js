90145a7148ecb43e692abdb44ac8b173c6e49f71
xtrycatchx <xtrycatchx@gmail.com>
2018-08-22 13:52:44 +0800

    added publish-subscribe messaging

--- number-buy-service/src/di/settingsModule.js
+++ number-buy-service/src/di/settingsModule.js
@@ -4,3 +4,7 @@ import * as config from '../config'
 export const mysqlConfigProvider = {
   mysqlConfig: asValue(config.mysqlConfig)
 }
+
+export const redisConfigProvider = {
+  redisConfig: asValue(config.redisConfig)
+}
